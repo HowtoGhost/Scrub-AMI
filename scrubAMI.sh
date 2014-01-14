2ad2d090529f59c9caaf258cd16b115a6239c884
Andy Boutte <andy@howtoinstallghost.com>
2014-01-13 20:03:46 -0800

    remove logs files
    * added removal of log files

--- scrubAMI.sh
+++ scrubAMI.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
 # scrubAMI.sh is used to clean our Amazon AMI before submission to the Marketplacei
-
+#
 # To run this on our AMI:
 # sudo wget -O - https://raw2.github.com/howtoinstallghost/Scrub-AMI/master/scrubAMI.sh | sudo bash
+
+# Stop Ghost
+forever stopall
 
 # Things to delete for Ghost
 
@@ -12,11 +15,10 @@ rm /var/www/ghost/content/data/*.db
 
 # Things to delete at OS level
 
+rm /var/log/cron*
+rm /var/log/secure*
+rm /var/log/messages*
+rm /home/ec2-user/.ssh/authorized_keys
+rm /root/.ssh/authorized_keys
 find / -name "authorized_keys" –exec rm –f {} \;
-sudo rm -r /home/ec2-user/.ssh/authorized_keys
-sudo rm -r /root/.ssh/authorized_keys
-find /root/.*history /home/*/.*history -exec rm -f {} \;
-history -c
-rm /etc/syslog.conf
-service rsyslog restart
 find /root/.*history /home/*/.*history -exec rm -f {} \;

218d9f945b86ee430d215edd9d67a817abba9b80
Andy Boutte <andy@howtoinstallghost.com>
2014-01-13 19:47:52 -0800

    First Commit
    First Commit

---
+++ scrubAMI.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
+# scrubAMI.sh is used to clean our Amazon AMI before submission to the Marketplacei
+
+# To run this on our AMI:
+# sudo wget -O - https://raw2.github.com/howtoinstallghost//master/scrubAMI.sh | sudo bash
+
+# Things to delete for Ghost
+rm /var/log/ghost
+rm /var/www/ghost/content/data/*.db
+
+# Things to delete at OS level
+
+find / -name "authorized_keys" –exec rm –f {} \;
+sudo rm -r /home/ec2-user/.ssh/authorized_keys
+sudo rm -r /root/.ssh/authorized_keys
+find /root/.*history /home/*/.*history -exec rm -f {} \;
+history -c
+rm /etc/syslog.conf
+service rsyslog restart
+rm ~/.bash_history ~/.zsh_history

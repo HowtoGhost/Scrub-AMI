5d70bb1c0591645035768a78c3705f2ce3c8e3fe
Andy Boutte <andy@howtoinstallghost.com>
2014-01-30 20:04:50 -0800

    added better documentation

--- scrubAMI.sh
+++ scrubAMI.sh
@@ -2,7 +2,7 @@
 # Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
 # scrubAMI.sh is used to clean our Amazon AMI before submission to the Marketplace
 #
-# To run this on our AMI:
+# Run the following from the ec2-user on our AMI:
 # HISTSIZE=0; sudo wget -O - https://raw2.github.com/howtoinstallghost/Scrub-AMI/master/scrubAMI.sh | sudo bash
 
 # Temporarily disable history
@@ -12,12 +12,10 @@ export HISTSIZE=0
 pm2 stop all
 
 # Things to delete for Ghost
-
 rm -r /var/log/ghost
 rm /var/www/ghost/content/data/*.db
 
 # Things to delete at OS level
-
 rm /var/log/cron*
 rm /var/log/secure*
 rm /var/log/messages*
@@ -26,6 +24,5 @@ rm /root/.ssh/authorized_keys
 find / -name "authorized_keys" –exec rm –f {} \;
 
 # Clear history last
-
 rm /root/.bash_history
 rm /home/ec2-user/.bash_history

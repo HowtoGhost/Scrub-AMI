bc9f172e9d7e5ca6bb6e693deac7567c782ec637
Andy Boutte <andy@howtoinstallghost.com>
2014-03-27 20:57:59 -0700

    deleting more log files and authorized_keys
    * removing log files from pm2
    * fixed the find command that removes authorized_keys

--- scrubAMI_Ghost.sh
+++ scrubAMI_Ghost.sh
@@ -2,7 +2,7 @@
 # Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
 # scrubAMI.sh is used to clean our Amazon AMI before submission to the Marketplace
 #
-# Run the following from the ec2-user on our AMI:
+# Run the following command (immediteialy after SSHing into server) from the ec2-user on our AMI:
 # HISTSIZE=0; sudo wget -O - https://raw2.github.com/howtoinstallghost/Scrub-AMI/master/scrubAMI.sh | sudo bash
 
 # Temporarily disable history
@@ -13,7 +13,10 @@ pm2 stop all
 
 # Things to delete for Ghost
 rm -r /var/log/ghost
-rm /.pm2/log
+rm /home/ghost/.pm2/logs/*
+rm /home/ghost/.pm2/pm2.log
+rm /root/.pm2/logs/*
+rm /root/.pm2/pm2.log
 rm /var/www/ghost/content/data/*.db
 
 # Things to delete at OS level
@@ -24,7 +27,7 @@ rm /var/log/nginx/*.log
 rm /home/ec2-user/.ssh/authorized_keys
 rm /home/ghost/.ssh/authorized_keys
 rm /root/.ssh/authorized_keys
-find / -name "authorized_keys" –exec rm –f {} \;
+find / -name "authorized_keys" -delete
 
 # Clear history last
 rm /root/.bash_history

b4ae7509b640e8bf9f8a3dd923a3c839de2d0f57
Andy Boutte <andy@howtoinstallghost.com>
2014-03-27 21:30:50 -0700

    converting to Discourse specifics

--- scrubAMI_Discourse.sh
+++ scrubAMI_Discourse.sh
@@ -2,28 +2,25 @@
 # Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
 # scrubAMI.sh is used to clean our Amazon AMI before submission to the Marketplace
 #
-# Run the following from the ec2-user on our AMI:
-# HISTSIZE=0; sudo wget -O - https://raw2.github.com/howtoinstallghost/Scrub-AMI/master/scrubAMI.sh | sudo bash
+# Run the following command (immediately after SSHing into server) from the ubuntu on our AMI:
+# HISTSIZE=0; sudo wget -O - https://raw2.github.com/howtoinstallghost/Scrub-AMI/master/scrubAMI_Discourse.sh | sudo bash
 
 # Temporarily disable history
 export HISTSIZE=0
 
 # Things to delete for Discourse
-rm -r /var/log/ghost
 rm /.pm2/log
 rm /var/www/ghost/content/data/*.db
 
 # Things to delete at OS level
+rm /var/log/cloud-init.log
 rm /var/log/cron*
 rm /var/log/secure*
 rm /var/log/messages*
-rm /var/log/nginx/*.log
-rm /home/ec2-user/.ssh/authorized_keys
-rm /home/ghost/.ssh/authorized_keys
+rm /home/ubuntu/.ssh/authorized_keys
 rm /root/.ssh/authorized_keys
-find / -name "authorized_keys" –exec rm –f {} \;
+find / -name "authorized_keys" -delete
 
 # Clear history last
 rm /root/.bash_history
-rm /home/ec2-user/.bash_history
-rm /home/discourse/.bash_history
+rm /home/ubuntu/.bash_history

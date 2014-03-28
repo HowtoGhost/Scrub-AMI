afb8aff66c5c8bab128a8e09ca6fdd308ed870d2
Andy Boutte <andy@howtoinstallghost.com>
2014-03-27 21:30:29 -0700

    updated comments and removing another log file

--- scrubAMI_Ghost.sh
+++ scrubAMI_Ghost.sh
@@ -2,8 +2,8 @@
 # Written by Andy Boutte and David Balderston of howtoinstallghost.com and allaboutghost.com
 # scrubAMI.sh is used to clean our Amazon AMI before submission to the Marketplace
 #
-# Run the following command (immediteialy after SSHing into server) from the ec2-user on our AMI:
-# HISTSIZE=0; sudo wget -O - https://raw2.github.com/howtoinstallghost/Scrub-AMI/master/scrubAMI.sh | sudo bash
+# Run the following command (immediately after SSHing into server) from the ec2-user on our AMI:
+# HISTSIZE=0; sudo wget -O - https://raw2.github.com/howtoinstallghost/Scrub-AMI/master/scrubAMI_Ghost.sh | sudo bash
 
 # Temporarily disable history
 export HISTSIZE=0
@@ -12,6 +12,7 @@ export HISTSIZE=0
 pm2 stop all
 
 # Things to delete for Ghost
+rm /var/log/cloud-init.log
 rm -r /var/log/ghost
 rm /home/ghost/.pm2/logs/*
 rm /home/ghost/.pm2/pm2.log
